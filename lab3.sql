adb654d2c6d015bb215b877de482f363f1002230
johnlohr47 <122938802+johnlohr47@users.noreply.github.com>
2023-09-13 16:31:46 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,26 +1,50 @@
 --Q2.1
 insert into student(s_email,s_name,major)
 values('s1@jmu.edu','s1','IA'),
-('s2@jmu.edu','s2','IA'),
-('s3@jmu.edu','s3','ISAT'),
-('s4@jmu.ed','s4','ISAT')
+      ('s2@jmu.edu','s2','IA'),
+      ('s3@jmu.edu','s3','ISAT'),
+      ('s4@jmu.ed','s4','ISAT')
 
 --Q2.2
 insert into professor(p_email,p_name,office)
+  
 values('p1@jmu.edu','p1','o1'),
 ('p2@jmu.edu','p2','o2')
 
 --Q2.3
 insert into course(c_number,c_name,room,p_email)
-values('c1','postresql','r1','p1@jmu.edu'),
-('c2','postresql','r2','p2@jmu.edu'),
-('c3','postresql','r3','p3@jmu,edu')
+  
+values('c1','postgresql','r1','p1@jmu.edu'),
+('c2','mon','r2','p2@jmu.edu'),
+('c3','twitter','r1','p1@jmu.edu')
 
 --Q2.4
 insert into enroll(s_email,c_number)
+  
 values('s1@jmu.edu','c1'),
-('s2@jmu.edu','c1'),
-('s3@jmu.edu','c1'),
-('s4@jmu.edu','c2'),
-('s2@jmu.edu','c3'),
-('s3@jmu.edu','c3')
+      ('s2@jmu.edu','c1'),
+      ('s3@jmu.edu','c1'),
+      ('s4@jmu.edu','c2'),
+      ('s2@jmu.edu','c3'),
+      ('s3@jmu.edu','c3')
+
+--Q2.5
+--insert professor first because the course table utilizes data from the professor table, such as professor email and professor name
+
+insert into professor(p_email,p_name,office)
+  
+values('p3@jmu.edu','p3','o3');
+
+insert into course(c_number,c_name,room,p_email)
+  
+values('c4','facebook','r1','p3@jmu.edu')
+
+--Q2.6
+--update first because it will still show p1
+update course 
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu';
+
+delete from professor
+where p_email = 'p1@jmu.edu'
+
